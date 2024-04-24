de4e0996421cd97c7de1d7e26292366c5b8d30d3
pasutinsung <pasut.run@gmail.com>
2024-04-24 22:56:44 +0700

    ConnectToDB

--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,20 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+  users: [
+    {
+      name: "admin",
+      email: "admin@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: true,
+    },
+    {
+      name: "P",
+      email: "user@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       name: "Dior handbag",
